302914d84c22e59a3e7b60963a767e7a9edcc93e
StefanCikarevic <stefancikarevic1996@gmail.com>
2022-09-06 12:48:11 +0200

    Get Tours

--- starter/app.js
+++ starter/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.status(200).json({ message: 'Hello from the server side' });
+// app.get('/', (request, response) => {
+//   response.status(200).json({ message: 'Hello from the server side' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (request, response) => {
+  response.status(200).json({ status: 'success', data: { tours: tours } });
 });
 
 const PORT = 3000;
